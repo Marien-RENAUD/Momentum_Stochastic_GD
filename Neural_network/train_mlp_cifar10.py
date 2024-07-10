6da824e4f9a3aecfe248b4f2239b0d8c1ea9c17b
marien.renaud@laposte.net <mrenaud@sirocco21.plafrim.cluster>
2024-07-10 10:44:47 +0200

    Improve the neural network architecture with batch normalization.

--- Neural_network/train_mlp_cifar10.py
+++ Neural_network/train_mlp_cifar10.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 import torchvision.transforms as t
 import os
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 # define network structure 
-net = nn.Sequential(nn.Linear(3 * 32 * 32, 1000), nn.ReLU(), nn.Linear(1000, 200), nn.ReLU(), nn.Linear(200, 40), nn.ReLU(), nn.Linear(40, 10))
+net = nn.Sequential(nn.Linear(3 * 32 * 32, 1024), nn.BatchNorm1d(1024), nn.ReLU(), nn.Linear(1024, 512), nn.BatchNorm1d(512), nn.ReLU(), 
+                    nn.Linear(512, 256), nn.BatchNorm1d(256), nn.ReLU(), nn.Linear(256, 128), nn.BatchNorm1d(128), nn.ReLU(), nn.Linear(128, 10)).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr = 0.01, momentum=0.9)
 
@@ -15,25 +18,34 @@ normalize = t.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 flatten =  t.Lambda(lambda x:x.view(-1))
 
 transform_list = t.Compose([to_tensor, normalize, flatten])
-train_set = torchvision.datasets.CIFAR10(root='/beegfs/mrenaud/Momentum_Stochastic_GD/dataset', train=True, transform=transform_list, download=False)
-test_set = torchvision.datasets.CIFAR10(root='/beegfs/mrenaud/Momentum_Stochastic_GD/dataset', train=False, transform=transform_list, download=False)
+train_set = torchvision.datasets.CIFAR10(root='/beegfs/mrenaud/Momentum_Stochastic_GD/dataset', train=True, transform=transform_list, download=True)
+test_set = torchvision.datasets.CIFAR10(root='/beegfs/mrenaud/Momentum_Stochastic_GD/dataset', train=False, transform=transform_list, download=True)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=64)
 
+# To save the trajectory
+
+weights_trajectory = []
+loss_trajectory = []
+gradients_trajectory = []
+
 # === Train === ###
 net.train()
 
+n_epoch = 3
+
 # train loop
-for epoch in range(3):
+for epoch in range(n_epoch):
     train_correct = 0
     train_loss = 0
     print('Epoch {}'.format(epoch))
     
     # loop per epoch 
     for i, (batch, targets) in enumerate(train_loader):
-
+        batch = batch.to(device)
         output = net(batch)
+        targets = targets.to(device)
         loss = criterion(output, targets)
 
         optimizer.zero_grad()
@@ -55,8 +67,9 @@ net.eval()
 
 # loop, over whole test set
 for i, (batch, targets) in enumerate(test_loader):
-    
+    batch = batch.to(device)
     output = net(batch)
+    targets = targets.to(device)
     pred = output.max(1, keepdim=True)[1]
     test_correct += pred.eq(targets.view_as(pred)).sum().item()
     
