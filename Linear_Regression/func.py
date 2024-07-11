6085eeb7173374528f1b802cacd33f77112cb7b4
J-Hermant <julien.hermant@math.u-bordeaux.fr>
2024-07-11 15:48:38 +0200

    change

--- Linear_Regression/func.py
+++ Linear_Regression/func.py
@@ -3,9 +3,9 @@ import numpy.random as nprandom
 rng = np.random.default_rng()
 def features(d,N,prompt_random_matrix = None,prompt_cov_matrix = None,prompt_bias = None):
     if prompt_bias != None:
-        b = prompt_bias
+        bias = prompt_bias
     else:
-        b = nprandom.normal(nprandom.uniform(-N,N),N,N)
+        bias = nprandom.normal(nprandom.uniform(-N,N),N,N)
     if prompt_cov_matrix != None:
         cov_matrix = prompt_cov_matrix
         features_matrix = nprandom.multivariate_normal(np.zeros(d),Cov_matrix,N)
@@ -17,7 +17,7 @@ def features(d,N,prompt_random_matrix = None,prompt_cov_matrix = None,prompt_bia
             random_matrix = nprandom.uniform(-1,1,(d,d))  
         cov_matrix = np.dot(random_matrix,random_matrix.T)
         features_matrix = nprandom.multivariate_normal(np.zeros(d),cov_matrix,N)
-        return features_matrix,b
+        return features_matrix,bias
 def f(x,features_matrix,bias):
     return 0.5*np.mean((np.dot(features_matrix,x)-bias)**2)
 def f_sample(x,N,features_matrix,bias):
@@ -26,7 +26,7 @@ def grad_f(x,N,features_matrix,bias):
     grad =(np.dot(features_matrix,x)-bias).reshape(N,1)*features_matrix
     gradient = np.sum(grad,axis=0)/N
     return gradient
-def grad_sto_f(x,N,n_sample,batch_size,features_matrix,bias):
+def grad_sto_f(x,d,N,n_sample,batch_size,features_matrix,bias):
     vec = np.tile(np.arange(N),n_sample).reshape(n_sample,N)
     grad =np.moveaxis((np.dot(features_matrix,x)-bias.reshape(N,1)),0,1).reshape(n_sample,N,1)*features_matrix
     batch_index = rng.permuted(vec,axis=1)[:,:batch_size] 
@@ -46,15 +46,20 @@ def GD(x_0,L,n_iter,d,N,features_matrix,bias):
         f_GD[i+1] = f(x_GD[i+1,:],features_matrix,bias)
     return f_GD
 
-def SGD(x_0,L_max,n_iter,n_sample,d,batch_size,N,features_matrix,bias):
+def SGD(x_0,L_max,n_iter,n_sample,d,batch_size,N,features_matrix,bias,random_init = False):
     x_SGD = np.empty((n_iter,d,n_sample))
     f_SGD = np.empty((n_iter,n_sample))
-    x_SGD[0,:,:] = x_0.reshape(d,1)
-    f_SGD[0,:] = f(x_0,features_matrix,bias)
+    if random_init == True:
+        x_SGD[0,:,:] = nprandom.normal(0, d,(d,n_sample))
+        f_sample(x_0,N,features_matrix,bias)
+    else:
+        x_SGD[0,:,:] = x_0.reshape(d,1)
+        f_SGD[0,:] = f(x_0,features_matrix,bias)
+   
     step = 1/L_max
 
     for i in range(n_iter-1):
-        gradient_sto = grad_sto_f(x_SGD[i,:,:],N,n_sample,batch_size,features_matrix,bias)
+        gradient_sto = grad_sto_f(x_SGD[i,:,:],d,N,n_sample,batch_size,features_matrix,bias)
         x_SGD[i+1,:,:] = x_SGD[i,:,:] - step*gradient_sto
         f_SGD[i+1,:] = f_sample(x_SGD[i+1,:],N,features_matrix,bias)
     return f_SGD
@@ -79,24 +84,32 @@ def NAG(x_0,mu,L,n_iter,d,N,features_matrix,bias):
 
     return f_nag
     
-def SNAG(x_0,mu,L,rho,n_iter,n_sample,d,batch_size,N,features_matrix,bias):
-    x_snag = np.empty((n_iter_gen,d,n_sample))
-    y_snag = np.empty((n_iter_gen,d,n_sample))
-    z_snag =  np.empty((n_iter_gen,d,n_sample))
-    f_snag =  np.empty((n_iter_gen,n_sample))
+def SNAG(x_0,mu,L,rho,n_iter,n_sample,d,batch_size,N,features_matrix,bias,random_init = False):
 
-    x_snag[0,:,:] = x_0.reshape(d,1)
-    y_snag[0,:,:] = x_0.reshape(d,1)
-    z_snag[0,:,:] = x_0.reshape(d,1)
-    f_snag[0,:] =  f(x_0,features_matrix,bias)
+    x_snag = np.empty((n_iter,d,n_sample))
+    y_snag = np.empty((n_iter,d,n_sample))
+    z_snag =  np.empty((n_iter,d,n_sample))
+    f_snag =  np.empty((n_iter,n_sample))
+    if random_init == False:
+        x_snag[0,:,:] = x_0.reshape(d,1)
+        y_snag[0,:,:] = x_0.reshape(d,1)
+        z_snag[0,:,:] = x_0.reshape(d,1)
+        f_snag[0,:] =  f(x_0,features_matrix,bias)
+
+    else:
+        x_0 = nprandom.normal(0, d,(d,n_sample))
+        x_snag[0,:,:] =  x_0
+        y_snag[0,:,:] = x_0
+        z_snag[0,:,:] =  x_0
+        f_snag[0,:] =  f_sample(x_0,N,features_matrix,bias)
 
     # Params
     step = 1/(L*rho)
     eta = 1/(np.sqrt(mu*L)*rho)
     beta = 1-np.sqrt( (mu/L) )/rho
     alpha = 1/(1+(1/rho)*np.sqrt(mu/L))
-    for i in range(1,n_iter_gen):
-        gradient_sto = grad_sto_f(y_snag[i-1,:,:],N,n_sample,batch_size,features_matrix,bias)
+    for i in range(1,n_iter):
+        gradient_sto = grad_sto_f(y_snag[i-1,:,:],d,N,n_sample,batch_size,features_matrix,bias)
 
         x_snag[i,:,:] = y_snag[i-1,:,:] - step*gradient_sto
         z_snag[i,:,:] = beta*z_snag[i-1,:,:] + (1-beta)*y_snag[i-1,:,:] - eta*gradient_sto
--- Linear_Regression/vizualisation.py
+++ Linear_Regression/vizualisation.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 from func import *
 ## Où importer les modules?
 
-d,N=10,10
-n_sample = 10
-batch_size = 10
-n_iter=100
+d,N=10,5 #choice of dimension d, number of functions N
+n_sample = 10 #number of parellel occurences of stochastic algorithms
+batch_size = 5 #size of batch
+n_iter=10**3
+
 features_matrix,bias = features(d,N)
 vec_norm= (features_matrix**2).sum(axis=1)
-L_max = vec_norm.max()
+L_max = vec_norm.max() ### MANQUE L_max dependant de batch size pour SGD
 x_0 = np.zeros(d)
 mu = nplinalg.eig(np.dot(features_matrix,features_matrix.T))[0].min()/N
 L = nplinalg.eig(np.dot(features_matrix,features_matrix.T))[0].max()/N
-rho = N
+rho = N/batch_size
 
 f_nag = NAG(x_0,mu,L,n_iter,d,N,features_matrix,bias)
 f_gd = GD(x_0,L,n_iter,d,N,features_matrix,bias)
@@ -25,10 +26,15 @@ f_snag = SNAG(x_0,mu,L,rho,n_iter,n_sample,d,batch_size,N,features_matrix,bias)
 list = [f_gd,f_sgd,f_nag,f_snag]
 labels = ["GD", "Mean-SGD", "NAG", "SNAG"]
 plt.figure(figsize=(10,10))
+color = ["black","grey","red","orange"]
 for j in range(4):
-    mean,min,max = np.mean(list[j],axis=1),np.min(list[j],axis=1),np.max(list[j],axis=1)
-    plt.plot(np.log(mean,label=labels[j],color ="grey",lw=2))
-    if min != max:
-        plt.plot(np.log(min),color ="grey",linestyle ="--")
-        plt.plot(np.log(max),color ="grey",linestyle ="--")
-plt.show
+    col = color[j]
+    if len(list[j].shape) >1:
+        mean,min,max = np.mean(list[j],axis=1),np.min(list[j],axis=1),np.max(list[j],axis=1)
+        plt.plot(np.log(mean),label=labels[j],color =col,lw=2)
+        plt.plot(np.log(min),color =col,linestyle ="--")
+        plt.plot(np.log(max),color =col,linestyle ="--")
+    else:
+        plt.plot(np.log(list[j]),label=labels[j],color =col,lw=2)
+plt.legend()
+plt.show()
