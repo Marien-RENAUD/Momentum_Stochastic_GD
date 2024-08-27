f74ddd54e9899bbbec6a32ff887d83310737ce14
J-Hermant <julien.hermant@math.u-bordeaux.fr>
2024-08-27 17:33:54 +0200

    update figures

--- Linear_Regression/link_racoga-curvature.py
+++ Linear_Regression/link_racoga-curvature.py
@@ -36,31 +36,15 @@ for i in range(precision):
         curvature[i,j] = torch.matmul(gradient.t(),torch.matmul(Hessian, gradient))/norm_gradient
 
 
-# plt.figure(figsize=(15,8))
-# plt.scatter(x_grid,y_grid,c = curvature)
-# print(Hessian)
-# plt.set_cmap('viridis')
-# plt.colorbar()
-# col_vec = np.ones(n_ech)
-# col_vec[np.where(vec_ech<0)] = 0
-# plt.scatter(vec_x[:,0],vec_x[:,1],c=vec_ech)
-# print(features_matrix)
-# plt.plot(np.array([0,features_matrix[0][0]]),np.array([0,features_matrix[0][1]]))
-# plt.plot(np.array([0,features_matrix[1][0]]),np.array([0,features_matrix[1][1]]))
-# plt.xticks((-1,0,1))
-# plt.yticks((-1,0,1))
-# plt.set_cmap('gist_heat')
-# plt.colorbar()
-# plt.show()
-
 
 plt.figure(figsize=(15, 8))
-
+number_size = 15
 # Premier scatter plot avec courbure
 scatter1 = plt.scatter(x_grid, y_grid, c=curvature, cmap='viridis', label='Curvature')
 cbar1 = plt.colorbar(scatter1, orientation='vertical')
 cbar1.set_label('Curvature')
 cbar1.set_ticks((torch.min(curvature),torch.max(curvature)))
+cbar1.ax.tick_params(labelsize=number_size) 
 cbar1.ax.yaxis.set_ticks_position('left')
 cbar1.ax.yaxis.set_label_position('left')
 cbar1.ax.set_position([0.05, 0.15, 0.02, 0.7])
@@ -73,6 +57,7 @@ print("Hessian Matrix:\n", Hessian)
 # Deuxième scatter plot avec vecteurs de données
 scatter2 = plt.scatter(vec_x[:, 0], vec_x[:, 1], c=vec_ech, cmap='gist_heat', label='Vector Data')
 cbar2 = plt.colorbar(scatter2, orientation='vertical')
+cbar2.ax.tick_params(labelsize=number_size) 
 cbar2.set_ticks([torch.min(vec_ech), torch.max(vec_ech)]) 
 cbar2.set_label('RACOGA')
 
@@ -81,8 +66,8 @@ plt.arrow(0,0, features_matrix[0, 0], features_matrix[0, 1],head_width=0.02, col
 plt.arrow(0,0, features_matrix[1, 0], features_matrix[1, 1],head_width=0.02, color='purple', lw=2, label='Feature 2')
 
 # Personnalisation des axes
-plt.xticks(np.linspace(-1, 1, num=3))  # Ticks à -1, 0, 1
-plt.yticks(np.linspace(-1, 1, num=3))
+plt.xticks(np.linspace(-1, 1, num=3), fontsize=number_size)  # Ticks à -1, 0, 1
+plt.yticks(np.linspace(-1, 1, num=3), fontsize=number_size)
 plt.xlim(-1, 1)
 plt.ylim(-1, 1)
 
