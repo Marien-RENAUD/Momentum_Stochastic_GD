004bf7902a4d6ed33fcbd8749d8ce61e1fc0d39a
Julien Hermant <jhermant@sirocco25.plafrim.cluster>
2024-09-09 16:50:48 +0200

    Relu replaced softplus mlp

--- Neural_network/data_sphere_generation.py
+++ Neural_network/data_sphere_generation.py
@@ -23,6 +23,7 @@ d = 32*32*3        # Dimension
 n_points = 60000  # Number of points to generate
 
 data = points_sur_sphere_torch(d, n_points)
+
 # Very simple label: one hemisphere is label "0", the other is label "1"
 labels = torch.zeros(n_points,dtype = torch.long)
 labels[torch.where((data[:,0]<0))]  =1
@@ -33,8 +34,10 @@ train_size = 50000
 test_size =  10000
     # Split dataset between training set and test set
 torch.manual_seed(42)
-train_dataset, test_dataset = torch.utils.data.random_split(data, [train_size, test_size])
-torch.manual_seed(42)
-train_labels, test_labels = torch.utils.data.random_split(labels, [train_size, test_size])
-torch.save({'data': train_dataset, 'labels': train_labels}, '/beegfs/jhermant/Momentum_Stochastic_GD/dataset/sphere/train_dataset_sphere.pth')
-torch.save({'data': test_dataset, 'labels': test_labels}, '/beegfs/jhermant/Momentum_Stochastic_GD/dataset/sphere/test_dataset_sphere.pth')
+train_data = data[:50000]
+train_labels = labels[:50000] 
+test_data = data[50000:]
+test_labels = labels[50000:]
+
+torch.save({'data': train_data, 'labels': train_labels}, '/beegfs/jhermant/Momentum_Stochastic_GD/dataset/sphere/train_dataset_sphere.pth')
+torch.save({'data': test_data, 'labels': test_labels}, '/beegfs/jhermant/Momentum_Stochastic_GD/dataset/sphere/test_dataset_sphere.pth')
