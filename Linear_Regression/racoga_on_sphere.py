92516a80f7350bab81a206a9f8ecabed2e44bee3
J-Hermant <julien.hermant@math.u-bordeaux.fr>
2024-09-16 10:06:04 +0200

    add figures racoga on sphere

--- Linear_Regression/racoga_on_sphere.py
+++ Linear_Regression/racoga_on_sphere.py
@@ -3,29 +3,72 @@ import numpy.random as nprandom
 import matplotlib.pyplot as plt
 import torch 
 from linear_regression import *
+from argparse import ArgumentParser
 
+# Define Parser arguments
+parser = ArgumentParser()
+parser.add_argument('--data_type', type=str, default = "both", choices=["both","gaussian_mixture", "sphere"])
+parser.add_argument('--n_ech', type=int, default = 100)
+hparams = parser.parse_args()
+data_type = hparams.data_type
+n_ech = hparams.n_ech
 d=1000
 N=100
+label_size = 20
+legend_size = 15
+number_size = 15
+labelpad_x = -10
+if data_type == "both":
 
-nb_class = 10
-mean = torch.rand(nb_class,d) * 2 * d - d # random
-# mean = torch.eye(nb_class,d)*d**2
+    mean = torch.zeros(d)
+    features_matrix,bias = sphere_uniform(d, N)
+    bias = torch.zeros(N)
+    nb_class = None
+    ech = sphere_uniform(d,n_ech)[0]
+    racoga = torch.empty(n_ech)
+    for i in range(n_ech):
+        racoga[i] = racoga_computation(ech[i,:],N,features_matrix,bias)
+    
+    plt.figure(figsize=(10,5))
+    plt.subplot(121)
+    col = "deepskyblue"
+    plt.hist(racoga,bins = "sqrt",edgecolor=None,facecolor = col,density = True,alpha = 1)
+    plt.xlabel("RACOGA",fontsize = label_size, labelpad = labelpad_x)
+    plt.xticks((-0.05,0,0.1),["-0.05", "0", "0.1"], fontsize = number_size)
+    plt.yticks((0,20), fontsize = number_size)
 
-#uniform spherical
-# mean = torch.zeros(d)
-# features_matrix,bias = sphere_uniform(d, N)
-# nb_class = None
-
-
-mixture_prob = np.ones(nb_class)/nb_class
-# mean = (torch.diag(torch.cat((torch.ones(nb_class),torch.zeros(d-nb_class))))*500)[:nb_class,:] ### orthognal classes
-features_matrix,bias = features_gaussian_mixture(d,N,mean=mean,mixture_prob=mixture_prob)
-
-n_ech = 100
-ech = sphere_uniform(d,n_ech)[0]
-racoga = torch.empty(n_ech)
-
-for i in range(n_ech):
-    racoga[i] = racoga_computation(ech[i,:],N,features_matrix,bias)
-    print(racoga[i])
-print(racoga.min(),racoga.max(),racoga.mean())
+    nb_class = 10
+    mean = torch.rand(nb_class,d) * 2 * d - d # random
+    mixture_prob = np.ones(nb_class)/nb_class
+    features_matrix,bias = features_gaussian_mixture(d,N,mean=mean,mixture_prob=mixture_prob)
+    racoga = torch.empty(n_ech)
+    for i in range(n_ech):
+        racoga[i] = racoga_computation(ech[i,:],N,features_matrix,bias)
+    plt.subplot(122)
+    plt.hist(racoga,bins = "sqrt",edgecolor=None,facecolor = col,density = True,alpha = 1)
+    plt.xlabel("RACOGA",fontsize = label_size, labelpad = labelpad_x)
+    plt.xticks((0,4),["0", "4"], fontsize = number_size)
+    plt.yticks((0,1), fontsize = number_size)
+    plt.legend(fontsize = legend_size,frameon = False)
+elif data_type == "sphere":
+    mean = torch.zeros(d)
+    features_matrix,bias = sphere_uniform(d, N)
+    nb_class = None
+    ech = sphere_uniform(d,n_ech)[0]
+    racoga = torch.empty(n_ech)
+    for i in range(n_ech):
+        racoga[i] = racoga_computation(ech[i,:],N,features_matrix,bias)
+    plt.figure(figsize=(5,5))
+    plt.hist(racoga,bins = "sqrt")
+else:
+    nb_class = 10
+    mean = torch.rand(nb_class,d) * 2 * d - d # random
+    mixture_prob = np.ones(nb_class)/nb_class
+    features_matrix,bias = features_gaussian_mixture(d,N,mean=mean,mixture_prob=mixture_prob)
+    ech = sphere_uniform(d,n_ech)[0]
+    racoga = torch.empty(n_ech)
+    for i in range(n_ech):
+        racoga[i] = racoga_computation(ech[i,:],N,features_matrix,bias)
+    plt.figure(figsize=(5,5))    
+    plt.hist(racoga,bins = "sqrt")
+plt.savefig("figures/curvature/racoga_sphere.png")
